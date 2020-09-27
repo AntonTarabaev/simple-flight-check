3c2750210928ce7b09d5efd795a0a14d5115a6dc
Anton Tarabaev <aa.tarabaev@gmail.com>
2020-09-27 15:53:05 +0300

    create basic project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import './scss/styles.scss';
+
 import React from 'react';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 style={{ fontWeight: '300' }}>Hello # > dkfolasdf /$} dfsfasdfasd !%^&*#&$#@#(@)$</h1>
     </div>
   );
-}
+};
 
 export default App;
